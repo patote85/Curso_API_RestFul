af0e2a67556ceeb874ba5e657075e83751bb9f0b
patote85 <r.patote@gmail.com>
2021-02-08 21:43:01 -0300

    meu primeiro commit

---
+++ src/main/java/br/com/patoter/udemyapirestful/UdemyApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package br.com.patoter.udemyapirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyApiRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/patoter/udemyapirestful/UdemyApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.patoter.udemyapirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
